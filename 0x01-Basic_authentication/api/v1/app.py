0d1bebcfb20541832dc902b3648bddb97b7ae373
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-13 22:57:56 +0000

    BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,8 +3,6 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -26,6 +24,11 @@ def not_found(error) -> str:
 def unauthorized(error):
     """ Not Authorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
