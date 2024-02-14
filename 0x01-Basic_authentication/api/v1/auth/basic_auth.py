fa974b55cf5377c2576534a65ddc56a28c8285df
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-14 10:54:59 +0000

    BasicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,6 +14,12 @@ class BasicAuth(Auth):
     """Basic authentication implementation"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header for Basic Authentication.
+        Args:
+            authorization_header (str): The Authorization header value.
+        Returns:
+            str: The Base64 part of the Authorization header, or None if invalid.
+        """
         if authorization_header is None or type(authorization_header) is not str:
             return None
 
