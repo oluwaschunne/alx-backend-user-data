56594dc090c0d4e027e065d95673061db521c955
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-15 13:00:04 +0000

    BasicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,21 +9,13 @@ from models.user import User
 
 from .auth import Auth
 
+
 class BasicAuth(Auth):
     """Basic authentication implementation"""
 
-    def extract_base64_authorization_header(self, authorization_header):
-        """Base64 part of the Authorization header for Basic Authentication
-
-        Args:
-            authorization_header (str): The Authorization header value.
-
-        Returns:
-            The Base64 part of the Authorization header, or None if invalid.
-
-        """
-        if authorization_header is None or \
-                type(authorization_header) is not str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extract authorization header from authorization header"""
+        if authorization_header is None or type(authorization_header) is not str:
             return None
 
         if not authorization_header.startswith("Basic "):
@@ -34,6 +26,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
     ) -> str:
+        """Decode base64 authorization header"""
         if (
             base64_authorization_header is None
             or type(base64_authorization_header) is not str
@@ -44,12 +37,13 @@ class BasicAuth(Auth):
             val = base64.b64decode(base64_authorization_header).decode("utf-8")
             return val
 
-        except Exception:
+        except:
             return None
 
     def extract_user_credentials(
         self, decoded_base64_authorization_header: str
     ) -> Tuple[str, str]:
+        """Extract credentials from the authorization"""
         if (
             decoded_base64_authorization_header is None
             or type(decoded_base64_authorization_header) is not str
@@ -66,6 +60,7 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar("User"):
+        """Returns a User object from credentials"""
         if user_email is None or type(user_email) is not str:
             return None
         if user_pwd is None or type(user_pwd) is not str:
@@ -78,6 +73,7 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
+        """get the current user from the database"""
         authHead = self.authorization_header(request)
         if not authHead:
             return
