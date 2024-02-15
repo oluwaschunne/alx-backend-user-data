ba1c3017e56d1a83c7d324b06cba181cfb5f6c17
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-15 17:54:33 +0000

    SessionAuth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from os import getenv
 
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -32,14 +33,25 @@ def before_request() -> None:
         return
     if not auth.require_auth(
         request.path,
-        ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
+        [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ],
     ):
         return
 
-    if auth.authorization_header(request) is None:
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
         abort(401)
-    if auth.current_user(request) is None:
+    cur = auth.current_user(request)
+    if cur is None:
         abort(403)
+
+    request.current_user = cur
 
 
 @app.errorhandler(404)
