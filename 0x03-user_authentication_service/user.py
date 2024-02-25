f57305c47fbbd60fc68b6163b87e9b7f3dbd324b
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-25 09:08:58 +0000

    UserAuth

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -24,7 +24,7 @@ class User(Base):
     """
     __tablename__ = "users"
     id = Column("id", Integer, primary_key=True, autoincrement=True)
-    email = Column("email", String(128), nullable=False)
-    hashed_password = Column("hashed_password", String(128), nullable=False)
-    session_id = Column("session_id", String(128), nullable=True)
-    reset_token = Column("reset_token", String(128), nullable=True)
+    email = Column("email", String(250), nullable=False)
+    hashed_password = Column("hashed_password", String(250), nullable=False)
+    session_id = Column("session_id", String(250), nullable=True)
+    reset_token = Column("reset_token", String(250), nullable=True)
